130a1ffbc097e3143c0de6f9b482a1c62f22eebc
rck109d <rck109d@ac69e85b-3713-e2aa-b8c9-542b59cb943b>
2010-04-01 15:29:35 +0000

    setup window added, transitions as well

--- HCIAutoPoolScoring/src/AutoPoolScorer.java
+++ HCIAutoPoolScoring/src/AutoPoolScorer.java
@@ -5,12 +5,14 @@ public class AutoPoolScorer {
 	/**
 	 * @param args
 	 */
+	static MainWindow mainWindow = null;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				new MainWindow();
+				mainWindow = new MainWindow();
 			}
 		});
 	}

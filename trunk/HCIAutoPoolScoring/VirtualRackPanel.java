2587de3ffb8c0e3e313e63bc37869ac098fed068
pdbryan <pdbryan@ac69e85b-3713-e2aa-b8c9-542b59cb943b>
2010-04-01 06:12:50 +0000

    Updated GUI virtual rack ball placement.  Balls now appear in the middle of the frame and are directly incident upon one another.  Also added mechanisms to move balls from the virtual rack to the player's detailed panel area.

--- trunk/HCIAutoPoolScoring/VirtualRackPanel.java
+++ trunk/HCIAutoPoolScoring/VirtualRackPanel.java
@@ -1,116 +1,193 @@
 import javax.swing.*;
 import javax.imageio.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 
-public class VirtualRackPanel extends JPanel {
+public class VirtualRackPanel extends JPanel implements MouseListener {
 
 	private JPanel panel = null;
 
 	private int panel_width;
 	private int panel_height;
 
-	private int w, h;
-	private BufferedImage ball_images;
+	private int ball_width;
+	private int ball_height;
+	
+	private final int SCALED_DIMENSIONS = 25;
 
-	private final int BALL_DIM = 30;
+	private boolean [] ball_displayed;
+	private BufferedImage[] ball_images;
+	private Point[] ball_points;	
 
 	public VirtualRackPanel(int width, int height) {
 		panel_width = width;
 		panel_height = height;
-		//ball_images = new BufferedImage[15];
+		ball_images = new BufferedImage[16];
+		ball_width = -1;
+		ball_height = -1;
+		ball_points = new Point[16];
+		ball_displayed = new boolean[16];
+		
+		for( int i = 0; i <= 15; i++) {
+			ball_images[i] = null;
+			ball_points[i] = null;
+			ball_displayed[i] = true;
+		}
+		
 		setDoubleBuffered(true);
 		setPreferredSize(new Dimension(panel_width, panel_height));
+		addMouseListener(this);
+		
+		loadBalls();
 	}
+	
+	public void loadBalls() {		
+	
+		for (int i = 1; i <= 15; i++) {
+			String imageSrc = null;
+			try {
+				imageSrc = "images" + File.separator + "Ball";
+				if (i < 10)
+					imageSrc += "0";
+				imageSrc += Integer.toString(i) + ".png";
+				
+				BufferedImage img = ImageIO.read(new FileInputStream(imageSrc));
+				
+				if (ball_width == -1) {
+					ball_width = img.getWidth();
+				} else if (ball_width != img.getWidth()) {
+					throw new Exception("All ball images must have the same dimension(width) " + imageSrc);
+				}
+				
+				if (ball_height == -1) {
+					ball_height = img.getHeight();
+				} else if (ball_height != img.getHeight()) {
+					throw new Exception("All ball images must have the same dimension(height) " + imageSrc);
+				}
+				
+				ball_images[i] = img;
+				
+			} catch (IOException e) {
+				System.out.println("Error reading file " + imageSrc);
+			} catch (Exception e) {
+				//die painfully
+			}
+		}
+	}
+	
+	public BufferedImage getBallImg(int i) {
+		if (i == 0) {
+			System.out.println("Cannot retreive image for ball 0 (it doesn't exist)");
+		}
+		return ball_images[i];
+	}
+	
+	private void calculateBallPositions() {
+		
+		int width = getWidth();
+		int height = getHeight();
+		Point middlePoint = new Point(width/2, height/2);		
+		
+		ball_points[5] = new Point(middlePoint.x, middlePoint.y - SCALED_DIMENSIONS/4);
+		ball_points[9] = bottomRightOf(ball_points[5]);
+		ball_points[8] = bottomLeftOf(ball_points[5]);
+		ball_points[2] = topLeftOf(ball_points[5]);
+		ball_points[3] = topRightOf(ball_points[5]);
+		ball_points[1] = topRightOf(ball_points[2]);
+		ball_points[4] = bottomLeftOf(ball_points[2]);
+		ball_points[6] = bottomRightOf(ball_points[3]);
+		ball_points[7] = bottomLeftOf(ball_points[4]);
+		ball_points[8] = bottomRightOf(ball_points[4]);
+		ball_points[10] = bottomRightOf(ball_points[6]);
+		ball_points[11] = bottomLeftOf(ball_points[7]);
+		ball_points[12] = bottomRightOf(ball_points[7]);
+		ball_points[13] = bottomRightOf(ball_points[8]);
+		ball_points[14] = bottomRightOf(ball_points[9]);
+		ball_points[15] = bottomRightOf(ball_points[10]);
+				
+		// now that all the points have been calculated, update positions to be relative
+		// to the middle of the image
+		for (int i = 1; i <= 15; i++) {
+			if (ball_points[i] != null) {
+				offsetImg(ball_points[i]);
+			}
+		}	
+	}
+		
+	private Point topLeftOf(Point p) {
+		double dy = Math.cos(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		double dx = Math.sin(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		Point np = new Point(p.x - (int)dx, p.y - (int)dy);	
+		return np;
+	}
+	
+	private Point bottomLeftOf(Point p) {
+		double dy = Math.cos(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		double dx = Math.sin(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		Point np = new Point(p.x - (int)dx, p.y + (int)dy);	
+		return np;
 
-	private void loadBI(String imageSrc) {
-		try {
-			ball_images= ImageIO.read(new FileInputStream(imageSrc));
-			w = (ball_images).getWidth(null);
-			h = ball_images.getHeight(null);
-		} catch (IOException e) {
-			System.out.println("Image could not be read");
+	}
+	private Point topRightOf(Point p) {
+		double dy = Math.cos(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		double dx = Math.sin(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		Point np = new Point(p.x + (int)dx, p.y - (int)dy);	
+		return np;
+	}
+	
+	private Point bottomRightOf(Point p) {
+		double dy = Math.cos(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		double dx = Math.sin(30 * Math.PI / 180) * SCALED_DIMENSIONS;
+		Point np = new Point(p.x + (int)dx, p.y + (int)dy);	
+		return np;
+	}
+	
+	private void offsetImg(Point p) {		
+		p.translate(-SCALED_DIMENSIONS/2, -SCALED_DIMENSIONS/2);
+	}
+	
+	public void removeBall(int i) {
+		if (i > 15 || i < 1) {
+			System.out.println("Balls must be between 1-15");
+		}
+		ball_displayed[i] = false;
+		repaint();
+	}
+	
+	public boolean ballAlreadySunk(int ball) {
+		return !ball_displayed[ball];
+	}
+	
+	public void newRack() {
+		for (int i = 1; i <= 15; i++) {
+			ball_displayed[i] = true;
 		}
 	}
 
-	//TODO come back and make this look better...
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-
-		int x_start, y_start;
-		// int OFFSET = 24;
-		int X_OFFSET = 34;
-		int Y_OFFSET = 24;
-
-		// head of the rack
-		x_start = getWidth() / 2;
-		y_start = 0;
-		loadBI("images/Ball01.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		// row 2
-		x_start -= (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		y_start += (int) (0.5 * (BALL_DIM + Y_OFFSET));
-		loadBI("images/Ball02.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start += 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball03.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		// row 3
-		x_start += (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		y_start += (int) (0.5 * (BALL_DIM + Y_OFFSET));
-		loadBI("images/Ball05.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start -= 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball08.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start -= 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball04.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		// row 4
-		x_start -= (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		y_start += (int) (0.5 * (BALL_DIM + Y_OFFSET));
-		loadBI("images/Ball06.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start += 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball07.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start += 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball09.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start += 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball10.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		// row 5
-		x_start += (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		y_start += (int) (0.5 * (BALL_DIM + Y_OFFSET));
-		loadBI("images/Ball15.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start -= 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball14.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start -= 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball13.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start -= 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball12.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
-
-		x_start -= 2 * (int) (0.5 * (BALL_DIM + X_OFFSET) * Math.atan(Math.PI / 6));
-		loadBI("images/Ball11.png");
-		g.drawImage(ball_images, x_start, y_start, x_start + BALL_DIM, y_start + BALL_DIM, 0, 0, w, h, null);
+		
+		calculateBallPositions();
+		for (int i = 1; i <= 15; i++) {
+			if (ball_points[i] != null && ball_displayed[i]) {
+				Point p = ball_points[i];
+				g.drawImage(ball_images[i], p.x, p.y, p.x + SCALED_DIMENSIONS, p.y + SCALED_DIMENSIONS, 
+						0, 0, ball_width, ball_height, null);	
+			}
+		}
 	}
+	
+	public void mousePressed(MouseEvent e) {
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+	}
+	
+	public void mouseEntered(MouseEvent e) { }
+	
+	public void mouseExited(MouseEvent e) { }
+
+	public void mouseReleased(MouseEvent e) { }	
 }

d08ebbe85cc72d253583f48dc2c5ec778d2dfdf6
rck109d <rck109d@ac69e85b-3713-e2aa-b8c9-542b59cb943b>
2010-04-01 19:04:05 +0000

    Send code, make sure to use args

---
+++ HCIAutoPoolScoring/src/wizard/Send.java
@@ -0,0 +1,34 @@
+package wizard;
+
+import java.io.*;
+import java.net.*;
+
+class Send
+{
+   public static void main(String args[]) throws Exception
+   {
+       BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+       while(true) {
+	   DatagramSocket clientSocket = new DatagramSocket();
+	   //InetAddress IPAddress = InetAddress.getByName("localhost");
+
+	   //byte[] inetAddr = { 128, 61, 24, 154 };
+	   InetAddress IPAddress = InetAddress.getByName(args[0]);
+
+	   byte[] sendData = new byte[1024];
+	   byte[] receiveData = new byte[1024];
+	   System.out.print("Enter Command: ");
+	   String sentence = inFromUser.readLine();
+	   if (sentence.equalsIgnoreCase("miss")) {
+	      sentence = "16";
+	   } else if (sentence.equalsIgnoreCase("fault")) {
+	       sentence = "17";
+	   }
+	   sendData = sentence.getBytes();
+	   DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+	   clientSocket.send(sendPacket);
+	   clientSocket.close();
+       }
+   }
+}
